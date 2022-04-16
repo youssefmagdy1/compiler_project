91d78538018c4218fc2dab2b2087968b655b0a17
Youssef Magdy <youssef.magdy.775@gmail.com>
2022-04-16 23:39:48 +0200

    add testing file

---
+++ test/SimpleProgram.java
@@ -0,0 +1,13 @@
+package test;
+
+public class SimpleProgram {
+    public static void main(String[] args) {
+       int x = 5 ;
+       if (x == 6 ) {
+           x = 0;
+       }else
+       {
+           x = 1 ;
+       }
+    }
+}
---
+++ test/Test2.java
@@ -0,0 +1,18 @@
+package test;
+
+public class Test2 {
+    public static void main(String[] args) {
+        int x = 5 ;
+        for(int i=0 ; i < 5 ; i ++ )
+        {
+            x ++ ;
+        }
+        if (x > 8)
+        {
+            x= 1 ;
+        }else
+        {
+            x = 0;
+        }
+    }
+}
