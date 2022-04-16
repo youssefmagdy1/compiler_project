b9fe8440e303fd1c2328c64ddf886eb4ff3d2457
Youssef Magdy <youssef.magdy.775@gmail.com>
2022-04-16 23:15:18 +0200

    add snapshots and simple code for testing

--- SimpleProgram.java
+++ SimpleProgram.java
@@ -1,18 +1,11 @@
 public class SimpleProgram {
     public static void main(String[] args) {
-        int variable1 = 10 , variable2 =0 , variableList[] = { 0 , 1 , 2 ,3};
-
-        for (int i = 0; i < 15; i++) {
-            variable2 = variable1;
-            variable1++;
-        }
-
-        if(variable1==variable2){
-            variable1++;
-        }
-        else{
-            variable2++;
-        }
-
+       int x = 5 ;
+       if (x == 6 ) {
+           x = 0;
+       }else
+       {
+           x = 1 ;
+       }
     }
 }
